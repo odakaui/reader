8a4110154b3f9d2e9699b3ecfbe28760b9a2b561
Odaka Ui <31593320+odakaui@users.noreply.github.com>
2021-09-11 17:15:16 -0700

    Revert "Implement Tokenizer"
    This reverts commit 6390c2c4e4154f9beee1d6a47eebf67291d1e2c0.

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,17 @@
 use pyo3::prelude::*;
+use pyo3::types::IntoPyDict;
 
-#[derive(Debug)]
-struct Token {
-    sentence: String,
-    text: String,
-    pos: String,
-    lemma: String,
-}
+fn main() -> PyResult<()> {
+    Python::with_gil(|py| {
+        let sys = py.import("sys")?;
+        let version: String = sys.getattr("version")?.extract()?;
 
-#[derive(Debug)]
-struct Tokenizer {
-    nlp: PyObject,
-}
+        let locals = [("os", py.import("os")?)].into_py_dict(py);
+        let code = "os.getenv('USER') or os.getenv('USERNAME') or 'Unknown'";
+        let user: String = py.eval(code, None, Some(&locals))?.extract()?;
 
-impl Tokenizer {
-    fn new() -> Self {
-        Python::with_gil(|py| -> PyResult<Tokenizer> {
-            let spacy = PyModule::import(py, "spacy")?;
-            let tokenizer_module = PyModule::from_code(py, r#"
-
-def tokenizer(spacy):
-    nlp = spacy.load("ja_core_news_lg")
-    return nlp
-
-            "#, "tokenizer.py", "tokenizer")?;
-
-            let nlp: PyObject  = tokenizer_module.getattr("tokenizer")?.call1((spacy,))?.extract()?;
-
-            Ok(Tokenizer {
-                nlp,
-            })
-        }).unwrap()
-
-    }
-
-    fn tokenize(&self, sentence: &str) -> Vec<Token> {
-        Python::with_gil(|py| -> PyResult<Vec<Token>> {
-            let tokenizer = PyModule::from_code(py, r#"
-
-def tokenize(nlp, sentence):
-    doc = nlp(sentence)
-    return doc
-
-        "#, "tokenizer.py", "tokenizer")?;
-
-        let tokens: Vec<&PyAny> = tokenizer.getattr("tokenize")?.call1((self.nlp.as_ref(py), sentence))?.extract()?;
-
-        Ok(tokens.iter().map(|x| Token {
-                sentence: sentence.to_string(),
-                text: x.getattr("text").unwrap().to_string(),
-                pos: x.getattr("pos_").unwrap().to_string(),
-                lemma: x.getattr("lemma_").unwrap().to_string()
-                }).collect())
-
-        }).unwrap()
-    }
-}
-
-fn main() {
-    let tokenizer = Tokenizer::new();
-
-    let sentences = vec!["でも、方針も決まったし、もう私に迷いは無い！", "ベッドの上に立ち上がり騒いでいたら、またお母さんに怒られた。", "そして、私はそのままベッドに潜り込んで眠りにつく。良い夢が……陽信の夢が見られるといいな。"];
-
-    for sentence in sentences.iter() {
-        let tokens = tokenizer.tokenize(sentence);
-
-        println!("{:?}", sentence);
-        
-        for token in tokens.iter() {
-            println!("{:?} {:?} {:?}", token.text, token.lemma, token.pos);
-        }
-    }
-
-
-
+        println!("Hello {}, I'm Python {}", user, version);
+        Ok(())
+    })
 }
 
