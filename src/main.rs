29e9bc9dd8c6f464b283f8b570cf4aa55d962d44
Odaka Ui <31593320+odakaui@users.noreply.github.com>
2021-10-15 19:16:25 -0700

    Change binary name
    Add share_dir for both debug and release targets

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use std::cell::RefCell;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::rc::Rc;
 
 use app::launch_app;
@@ -16,10 +16,22 @@ pub mod app;
 pub mod application_state;
 pub mod database;
 
-pub fn main() -> Result<()> {
+#[cfg(debug_assertions)]
+fn share_dir() -> PathBuf {
     let resources = Path::new(env!("CARGO_MANIFEST_DIR")).join("resources");
 
-    let share_dir = resources.join("reader");
+    resources.join("reader")
+}
+
+#[cfg(not(debug_assertions))]
+fn share_dir() -> PathBuf {
+    let share = dirs::home_dir().expect("failed to open home directory").join(".local/share");
+
+    share.join("reader")
+}
+
+pub fn main() -> Result<()> {
+    let share_dir = share_dir();
 
     let database = Rc::new(RefCell::new(Database::new(&share_dir)?));
     let reader_state = database.borrow_mut().current()?;
