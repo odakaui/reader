36409424cb97f55ee4200cf20d4c3cb62cda991a
Odaka Ui <31593320+odakaui@users.noreply.github.com>
2021-10-20 12:18:02 -0700

    Implement test for Line

--- src/database/file/line.rs
+++ src/database/file/line.rs
@@ -18,3 +18,24 @@ impl Line {
         Ok(Line { sentence, words })
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_new() -> Result<()> {
+        let original = "なんとか今日を生き延びた僕は、帰りに七海さんと一緒に生活雑貨を扱うバラエティショップへと来ていた。";
+
+        let mut tokenizer = Tokenizer::new()?;
+        let line = Line::new(&mut tokenizer, original)?;
+
+        assert_eq!(line.sentence, original, "line.sentence {} is not equal to original {}.", line.sentence, original);
+        
+        let sentence = line.words.iter().fold(String::new(), |sentence, word| sentence + &word.tokens.iter().fold(String::new(), |word, token| word + &token.text));
+
+        assert_eq!(sentence, original, "sentence from words {} is not equal to original {}.", sentence, original);
+
+        Ok(())
+    }
+}
